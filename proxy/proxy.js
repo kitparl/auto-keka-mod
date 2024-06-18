e08baea85aa8d097cd4e90c2cd6581cbbbdabdfd
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-06-18 16:52:59 +0530

    added:

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require("body-parser");
+const fetch = require('node-fetch'); 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,29 +18,72 @@ app.use((req, res, next) => {
   next();
 });
 
+// Middleware to handle CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
+
+//   app.use('/proxy', async (req, res) => {
+//   const method = Object.keys(req.body).length === 1 ? 'GET' : 'POST';
+//   const url = 'https://techfino.keka.com' + req.url;
+
+//   const length = Object.keys(req.body).length;
+//   const token = req.body["Authorization"];
+//     delete req.body["Authorization"];
+
+//     await request({method, url, headers: { 
+//       Authorization: token,
+//       "Content-type": "application/json"
+//     }, body: JSON.stringify(req.body) }, function (error, response, body) {
+//       console.log("body+++++???>>>>", body);
+//       res.send(body);
+//     });
+// });
+
 
 app.use('/proxy', async (req, res) => {
   const method = Object.keys(req.body).length === 1 ? 'GET' : 'POST';
   const url = 'https://techfino.keka.com' + req.url;
 
-  const length = Object.keys(req.body).length;
   const token = req.body["Authorization"];
-    delete req.body["Authorization"];
+  delete req.body["Authorization"];
 
-    await request({method, url, headers: { 
+  const options = {
+    method,
+    headers: {
       Authorization: token,
-      "Content-type": "application/json"
-    }, body: JSON.stringify(req.body) }, function (error, response, body) {
-      console.log("body+++++???>>>>", body);
-      res.send(body);
-    });
-})
+      'Content-type': 'application/json',
+    },
+  };
+
+  if (method === 'POST') {
+    options.body = JSON.stringify(req.body);
+  }
+
+  try {
+    const response = await fetch(url, options);
+
+    // Check if the response is JSON
+    const contentType = response.headers.get("content-type");
+    let body;
+    if (contentType && contentType.includes("application/json")) {
+      body = await response.json();
+    } else {
+      body = await response.text();
+    }
+
+    return res.status(response.status).send(body);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Proxy server running on port 3000');
